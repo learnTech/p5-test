3cfb713eb31b24c9d6e50e1fa55f983562cebbe4
Swapy97 <gschaiderniklas@gmail.com>
2016-08-31 14:13:38 +0200

    Bouncing Boxes dont overlay text at the top

--- sketch.js
+++ sketch.js
@@ -11,17 +11,19 @@ function draw() {
     stroke(255);
     background(51);
 
+    for(var i=0;i<entities.length;i++){
+        entities[i].update();
+        entities[i].draw();
+    }
+
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
     textAlign(LEFT);
     text(width+"/"+height, 10, 20);
     text(round(frameRate())+" FPS", width-50, 20);
 
     textAlign(CENTER);
     text(entities.length, width/2, 20);
-
-    for(var i=0;i<entities.length;i++){
-        entities[i].update();
-        entities[i].draw();
-    }
 }
 
 function touchStarted(){
